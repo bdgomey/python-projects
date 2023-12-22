356d096339446735736caa4e83dea57c6861e833
Brian Gomes <67698602+bdgomey@users.noreply.github.com>
2023-12-22 10:53:59 -0600

    commit message

---
+++ FlaskResumeApp/react/frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const myFirstElement = <h1>Hello React!</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement);
+
+class Car {
+  constructor(name) {
+    this.brand = name;
+  }
+  Present(){
+    return 'Ihave a ' + this.brand
+  }
+}
+
+
+
+const myCar = new Car('Ford');
+myCar.Present();
